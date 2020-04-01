2e5cdcbf2386b97a77ed4c0a3ac17c029cd08e96
Lucio Chavez <luciobertinchavez@gmail.com>
2020-04-01 13:41:43 -0600

    Habilitate debugger

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -10,6 +10,10 @@ const override = css`
 `;
 
 class BusinessList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {notFoundMessage: ""}
+      }
     render() {
         return (
             <>
@@ -22,9 +26,20 @@ class BusinessList extends React.Component {
                 />
             </div>
             <div className="BusinessList"  >
-                {this.props.businesses.map(business => {
-                    return <Business business={business} key={business.id} />
-                }) }
+                { this.props.businesses.length !== 0 ? (
+                    this.props.businesses.map(business => {
+                        return <Business business={business} key={business.id} />
+                    })
+
+                ) : (
+
+                    <p>
+                        !No restaurants found¡ Did you search for food? 
+                        Try searching something like "sea food", "tacos", "meat", "beef", etc.
+                    </p>
+
+                )}
+                
             </div>
             </>
         )
