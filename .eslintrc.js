0a2963cb9dcd6b37a1d9f2010c3f7b150d0abd2b
Lucio Chavez <luciobertinchavez@gmail.com>
2020-08-11 16:26:26 -0500

    install eslint/typescript and frontend package plugins

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['airbnb-typescript'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};
