f9e1251b1c7106927a74d90f115db12eb1776018
Lucio Chavez <luciobertinchavez@gmail.com>
2020-08-14 17:00:37 -0500

    configure business request type correctly

--- packages/Back-End/functions/src/routes/businesses.ts
+++ packages/Back-End/functions/src/routes/businesses.ts
@@ -2,13 +2,12 @@
 import * as express from 'express';
 import axios from 'axios';
 const cors = require('cors');
-//import { Request, Response } from 'express';
 
 interface GetBusinessesRequest extends express.Request {
-	body: { 
-		term: string,
-		location: string,
-		sortBy: string, 
+	query: {
+		term: string;
+		location: string; 
+		sortBy: string;
 	}
 }
 
@@ -17,9 +16,8 @@ const businesses = express.Router();
 const getBusinesses = async (req : GetBusinessesRequest, res : express.Response) => {
 
 	const apiKey = 'AmX43N_jFBUWKFgYcSXFpRPSPQhxXwUjRx1odw35LY01H5xeIDAlHvPKRQ2vLs6lWFj0NeHq2X7cFRnaBjiQP4DZnacuMvQLzPjTCjytjX93i9mNOzT2NQzZ1eSPXHYx';
-
 	const {term, location, sortBy} = req.query
-
+	
 	if(term !== undefined && location !== undefined && sortBy !== undefined) {
 		const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`
 
@@ -39,7 +37,6 @@ const getBusinesses = async (req : GetBusinessesRequest, res : express.Response)
 	} else {
 		return res.status(400).json({error: 'term not provided'})
 	}
-
 }
 
 businesses.use(cors())
